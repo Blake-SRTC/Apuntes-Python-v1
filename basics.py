98cedf00f8fe44ae44281945659a423947ec518a
Saul Tapia <rodrigo.1010stc@gmail.com>
2021-07-14 21:45:28 -0400

    iniciando git

---
+++ basics.py
@@ -0,0 +1,29 @@
+#Tipos de datos
+v_bool1 = True
+v_bool2 = False
+v_int = 5
+v_float = 3.1415
+v_str = "hola"
+
+#Operadores numericos
+suma = 3 + 3
+resta = 4 - 2
+multi = 5 * 6
+divi = 9 / 3
+potencia = 2 ** 3
+modulo = 7 % 3 #resto
+div_entera =7 // 3 #cociente
+
+#Operadores de comparacion
+igualdad = (3 == 3)
+distinto = (4 != 4)
+mayor_que = (5 > 1)
+menor_que = (1 < 7)
+mayor_o_igual = (5 >= 5)
+menor_o_igual = (3 <= 3)
+
+#Operadores booleanos
+
+#and
+#or
+#not
