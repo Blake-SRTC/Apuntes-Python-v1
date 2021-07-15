886f3ab5df953a62fb6e02ce4a54110024e019e2
Saul Tapia <rodrigo.1010stc@gmail.com>
2021-07-14 21:56:18 -0400

    agregando bascis

---
+++ basics_2.py
@@ -0,0 +1,24 @@
+#Concatenar
+michis = "gato " + "michi"
+
+#Obtener longitud
+palabra = "edificio"
+longitud_palabra = len(palabra)
+
+#Posicion de caracter
+posicion = palabra[0]
+
+#Rebanada de string
+rebadana = "the boys"[2:6]
+
+#Mostrar datos
+print("impresion en consola")
+
+#Salida multiple
+print("hola","mundo") #agrega un espacio entre palabras
+
+#Lectura entrada de consola
+nombre = input("Ingresa tu nombre")
+
+#Entrada a numero
+edad = int(input("Ingresa tu edad"))
