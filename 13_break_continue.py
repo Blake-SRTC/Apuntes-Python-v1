0be1ccece075e151f1226cf5174418bf5f5a4db7
Saul Tapia <rodrigo.1010stc@gmail.com>
2021-07-15 14:50:35 -0400

    correciones

--- 13_break_continue.py
+++ 13_break_continue.py
@@ -1,9 +1,11 @@
 def run():
+    #Continue se salta la vuelta del ciclo for y va a la siguiente vuelta, no ejecuta lo que se encuentra debajo por el salto
     # for contador in range(1000):
     #     if contador % 2 != 0:
     #         continue
     #     print(contador)
 
+    #Break corta el ciclo for en el limite indicado
     # for i in range(10000):
     #     print(i)
     #     if i == 5678:
