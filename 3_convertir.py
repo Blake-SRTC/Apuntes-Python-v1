5b5955040d0397e94444579352bfdbc9275a1e5f
Saul Tapia <rodrigo.1010stc@gmail.com>
2021-07-14 22:37:17 -0400

    detalles

--- 3_convertir.py
+++ 3_convertir.py
@@ -7,3 +7,7 @@ numero_int = int(input("Ingrese un numero")) #Modificara el String a un int
 #Convertir de int a String
 numero_entero = int(input("Ingrese un numero"))
 numero_a_texto = str(numero_entero)
+
+#Convertir de decilam a entero
+numero_decimal = 4.5
+num_entero = int(numero_decimal) #resultado 4
